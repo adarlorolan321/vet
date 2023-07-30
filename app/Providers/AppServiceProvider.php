90a4d3f0ecc2cbcbcda650ba91d105cbf45754b4
Mark Alfred Macapagal <innsite.ph.22@gmail.com>
2023-07-30 10:04:50 +0800

    add https in web

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'dev') {
+            URL::forceScheme('https');
+            Request::setTrustedProxies([request()->getClientIp()], Request::HEADER_X_FORWARDED_PROTO);
+        }
     }
 }
